4facf1aacc28e23a926274915859e558d4f98062
popularkoju <webpk144@gmail.com>
2022-04-07 17:21:05 +0545

    fix

--- schoolmanagementbackend/schoolmanagement/lib/main.dart
+++ schoolmanagementbackend/schoolmanagement/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:schoolmanagement/Screens/Floors/FloorScreen.dart';
 
 void main() {
   runApp(const MyApp());
